1fc07e9e7793fdd5492a1edbbb29f9b508a3a8bd
Dmitriy <mr.dima337@gmail.com>
2025-09-08 16:27:07 +0300

    feat: implement hometask 2

--- lesson-05/src/App.tsx
+++ lesson-05/src/App.tsx
@@ -2,6 +2,13 @@ import { useState } from "react";
 const numbers = [1, 5, 10, 15, 20];
 const arrNum = numbers.map((n) => n * 10);
 console.log(arrNum);
+
+const names = ["Alice", "Bob", "Charlie", "Diana"];
+const arrObjNames = names.map((name) => {
+  return { name, length: name.length };
+});
+console.log(arrObjNames);
+
 function App() {
   return <></>;
 }
