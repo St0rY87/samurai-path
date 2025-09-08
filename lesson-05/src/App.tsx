5487d24213ae5af6b901ed5f22c7c96979d7dd07
Dmitriy <mr.dima337@gmail.com>
2025-09-08 16:21:33 +0300

    feat: implement hometask 1

---
+++ lesson-05/src/App.tsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+const numbers = [1, 5, 10, 15, 20];
+const arrNum = numbers.map((n) => n * 10);
+console.log(arrNum);
+function App() {
+  return <></>;
+}
+
+export default App;
