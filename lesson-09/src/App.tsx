91e870f0c90711fa00aa8135a33d329b1cbd36f7
Dmitriy <mr.dima337@gmail.com>
2025-09-22 23:36:03 +0300

    feat: implement task-1 of lesson-09

--- lesson-09/src/App.tsx
+++ lesson-09/src/App.tsx
@@ -1,7 +1,107 @@
 import { useState } from "react";
+import "./App.css";
+
+const tasks = [
+  {
+    id: 1,
+    title: "Купить продукты на неделю",
+    isDone: false,
+    addedAt: "1 сентября",
+    priority: 2,
+  },
+  {
+    id: 2,
+    title: "Полить цветы",
+    isDone: true,
+    addedAt: "2 сентября",
+    priority: 0,
+  },
+  {
+    id: 3,
+    title: "Сходить на тренировку",
+    isDone: false,
+    addedAt: "3 сентября",
+    priority: 1,
+  },
+  {
+    id: 4,
+    title: "Срочно отправить рабочий отчет",
+    isDone: false,
+    addedAt: "4 сентября",
+    priority: 4,
+  },
+  {
+    id: 5,
+    title: "Заплатить за коммунальные услуги",
+    isDone: false,
+    addedAt: "3 сентября",
+    priority: 3,
+  },
+];
+
+function Task({ task }) {
+  function definePriorityTask(num) {
+    switch (num) {
+      case 0:
+        return "#ffffff";
+      case 1:
+        return "#ffd7b5";
+      case 2:
+        return "#ffb38a";
+      case 3:
+        return "#ff9248";
+      case 4:
+        return "#ff6700";
+      default:
+        return "#ffffff";
+    }
+  }
+
+  function handleClick(id) {
+    alert(id);
+  }
+
+  return (
+    <li onClick={() => handleClick(task.id)}>
+      <article
+        className="task"
+        style={{
+          backgroundColor: definePriorityTask(task.priority),
+        }}
+      >
+        <p
+          style={{ textDecorationLine: task.isDone ? "line-through" : "none" }}
+        >
+          <b>Заголовок: </b>
+          {task.title}
+        </p>
+        <div>
+          <span>
+            <b>Статус </b>
+          </span>
+          <input type="checkbox" checked={task.isDone} />
+        </div>
+        <p>
+          <span>
+            <b>Дата создания задачи: </b>
+          </span>
+          {task.addedAt}
+        </p>
+      </article>
+    </li>
+  );
+}
 
 function App() {
-  return <div></div>;
+  if (tasks === null) return "Загрузка";
+  if (tasks.length === 0) return "Задачи отсутствуют";
+  return (
+    <ul>
+      {tasks.map((task) => (
+        <Task task={task} key={task.id} />
+      ))}
+    </ul>
+  );
 }
 
 export default App;
